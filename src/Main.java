8a8567b328b310312e76c24f12193687b20a3864
Eric Thomas <bad.llama.group@gmail.com>
2019-09-21 18:09:38 -0700

    Add HUD
    This adds the navigation bar and sets the app icon.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,42 @@
+import java.io.FileInputStream;
+
+import View.HUD;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application
+{
+    private BorderPane theWindow;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("View/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start( Stage primaryStage ) throws Exception
+    {
+        primaryStage.setTitle( "Food 4 Life" );
+        primaryStage.getIcons().add( new Image( new FileInputStream( "resources/icon.png" ) ) );
+
+        theWindow = new BorderPane();
+
+        // Create views
+        HUD theHUD = new HUD();
+
+        // Set initial view
+        theWindow.setTop( theHUD );
+
+        // Set alignment
+        BorderPane.setAlignment( theWindow, Pos.CENTER );
+
+        Scene theScene = new Scene( theWindow, 1000, 1000 );
+        primaryStage.setScene( theScene );
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main( String[] args )
+    {
+        launch( args );
     }
 }
